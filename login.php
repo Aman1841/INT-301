11c1ee59b9537a62e74f372b12da143bf95b9e4f
Aman Pratap Singh <37451982+Aman1841@users.noreply.github.com>
2020-04-10 10:58:27 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>login page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<font color ="white"><center><h1> GOVERMENT SENIOR SECONDRY SCHOOL ADAMPUR</h1></center></font>
+	<div id="frm">
+
+<h2>
+
+	<form action = "process.php" method= "POST">
+		USERNAME<input type="text" id="user" name = "user" />
+	    PASSWORD<input type="password" id="pass" name = "pass" />
+		<input type="submit" id="btn"  value="Login">
+	    <h5><a href="fp.php">fogot password</a></h5>
+	    
+
+   </form>
+	    </h2>
+	</div>
+
+</body>
+</html>
